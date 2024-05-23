17c9e00db19d468f8950d47dcdf1324b04400672
ShahEmaad <115357853+ShahEmaad@users.noreply.github.com>
2024-05-23 20:08:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Shah Emaad",
+  title: "Full-Stack Developer",
+  email: "shah.emaad121@gmail.com",
+  gitHub: "https://github.com/ShahEmaad",
+  instagram: "",
+  linkedIn: "https://www.linkedin.com/in/emaad-shah-b28579232/",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
